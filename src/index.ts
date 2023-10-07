879c4cea018ed59ef47988bd36254674fb17527d
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-10-07 15:39:38 -0500

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 /* eslint-disable import/first */
 import express from 'express';
+import cors from 'cors';
 import { getCharacterClassData } from './models/character-class/character-class';
 import { log } from './models/logger';
 
@@ -13,6 +14,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 (() => getCharacterClassData())();
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000', // Replace with your frontend's address. Use '*' to allow any origin (not recommended for production).
+        methods: ['GET', 'POST'], // Specify which methods are allowed
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+);
 
 app.use('/api/classes', classesRouter);
 
