2a60f37811ea5f063dad5123a99e5a435ed45529
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-10-09 11:11:16 -0500

    Add subrace description parsing

--- src/models/character-feature/character-race/subrace.ts
+++ src/models/character-feature/character-race/subrace.ts
@@ -1,18 +1,40 @@
+import { MediaWiki } from '../../media-wiki';
 import { ICharacterFeatureCustomizationOption } from '../feature-customization-option/types';
 
 export class CharacterSubrace implements ICharacterFeatureCustomizationOption {
     label: string;
+    description: string;
 
     constructor(
         public name: string,
         public content: string,
     ) {
         this.label = name;
+        this.description = this.initDescription();
+    }
+
+    // FIXME: This mostly works, but produces artifacts on a few subraces
+    initDescription(): string {
+        // Regular expression to capture content between double single quotes or within {{Q|...}}
+        const regex = /''(.*?)''|{{Q\|(.*?)(?:\|.*?)*?}}/;
+        const match = regex.exec(this.content);
+
+        if (!match) {
+            if (MediaWiki.stripMarkup(this.content) === this.content) {
+                return this.content;
+            }
+
+            throw new Error('Unable to parse description from content');
+        }
+
+        // Return the first non-null capturing group (either from ''...'' or from {{Q|...}})
+        return MediaWiki.stripMarkup(match[1] || match[2]).trim();
     }
 
     toJSON() {
         return {
             name: this.label,
+            description: this.description,
         };
     }
 }
