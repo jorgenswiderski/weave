8793b65f1788e48e0c4f274fffe45fb29586fc50
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-10-15 16:40:33 -0500

    Parse feat info including abilities

--- src/models/character-feature/features/character-feat.ts
+++ src/models/character-feature/features/character-feat.ts
@@ -1,6 +1,7 @@
 import { CharacterPlannerStep } from 'planner-types/src/types/character-feature-customization-option';
 import {
     GrantableEffect,
+    GrantableEffectSubtype,
     GrantableEffectType,
 } from 'planner-types/src/types/grantable-effect';
 import { error } from '../../logger';
@@ -9,6 +10,7 @@ import { CharacterFeatureCustomizable } from '../character-feature-customizable'
 import { PageLoadingState } from '../../page-item';
 import { PageNotFoundError } from '../../errors';
 import { CharacterFeature } from '../character-feature';
+import { CharacterFeatAbilities } from './character-feat-abilities';
 
 enum SubclassLoadStates {
     CHOICES = 'CHOICES',
@@ -34,15 +36,26 @@ export class CharacterFeat extends CharacterFeatureCustomizable {
         }
 
         const featPattern =
-            /\|{{anchor\|([^}]+)}} '''(.*?)''' \|\|([\s\S]*?)(?=\|-\n|\|-|$)/g;
+            /\|{{anchor\|([^}]+)}} '''(.*?)'''[^|]*?\|\|([\s\S]*?)(?=\|-\n|\|-|$)/g;
         const grantPattern = /[^*]\s*'''{{SAI\|(.*?)\|/g;
         const choicePattern = /\*\s*'''{{SAI\|(.*?)[|}]/g;
+
+        const abilityImprovementPatterns = [
+            /Your\s+{{Ability\|(\w+)}}\s+or\s+{{Ability\|(\w+)}}\s+increases\s+by\s+(\d+)/, // Matches: "Your Strength or Dexterity increases by 1"
+            /Increase your\s+{{Ability\|(\w+)}}\s+or\s+{{Ability\|(\w+)}}\s+by\s+(\d+)/, // Matches: "Increase your Strength or Dexterity by 1"
+            /Increase\s+your\s+{{Ability\|(\w+)}}\s+by\s+(\d+)/, // Matches: "Increase your Strength by 1"
+            /{{Ability\|(\w+)}}\s+increases\s+by\s+(\d+)/, // Matches: "Your Charisma increases by 1"
+        ];
 
         const feats: {
             [key: string]: {
                 grants: string[];
                 choices: string[];
                 description: string;
+                abilityImprovement?: {
+                    abilities: string[];
+                    points: number;
+                };
             };
         } = {};
 
@@ -50,6 +63,10 @@ export class CharacterFeat extends CharacterFeatureCustomizable {
             const featName = match[2].trim();
             const grants: string[] = [];
             const choices: string[] = [];
+            let abilityImprovement: {
+                abilities: string[];
+                points: number;
+            } | null = null;
 
             Array.from(match[3].matchAll(grantPattern)).forEach(
                 (abilityMatch) => {
@@ -63,17 +80,65 @@ export class CharacterFeat extends CharacterFeatureCustomizable {
                 },
             );
 
+            if (
+                featName === 'Ability Improvement' ||
+                featName === 'Resilient'
+            ) {
+                abilityImprovement = {
+                    abilities: [
+                        'Strength',
+                        'Dexterity',
+                        'Constitution',
+                        'Intelligence',
+                        'Wisdom',
+                        'Charisma',
+                    ],
+                    points: featName === 'Ability Improvement' ? 2 : 1,
+                };
+            } else {
+                abilityImprovementPatterns.forEach((pattern) => {
+                    if (abilityImprovement) {
+                        return;
+                    }
+
+                    const abilityImprovementMatch = match[3].match(pattern);
+
+                    if (abilityImprovementMatch) {
+                        const abilities = abilityImprovementMatch.slice(1, -1); // Exclude the last group which is the amount
+                        const points = parseInt(
+                            abilityImprovementMatch[
+                                abilityImprovementMatch.length - 1
+                            ],
+                            10,
+                        );
+
+                        if (abilities?.length > 0) {
+                            abilityImprovement = {
+                                abilities,
+                                points,
+                            };
+                        }
+                    }
+                });
+            }
+
             feats[featName] = {
                 grants,
                 choices,
                 description: MediaWiki.stripMarkup(match[3]).trim(),
+                abilityImprovement: abilityImprovement ?? undefined,
             };
         });
 
         this.choices = [
             await Promise.all(
                 Object.entries(feats).map(async ([name, data]) => {
-                    const { grants, choices: choiceTitles, description } = data;
+                    const {
+                        grants,
+                        choices: choiceTitles,
+                        description,
+                        abilityImprovement,
+                    } = data;
                     const fx = (
                         await Promise.all(
                             grants.map((pageTitle) =>
@@ -100,6 +165,34 @@ export class CharacterFeat extends CharacterFeatureCustomizable {
                             description,
                             type: GrantableEffectType.CHARACTERISTIC,
                         });
+                    }
+
+                    if (abilityImprovement) {
+                        if (abilityImprovement.abilities.length > 1) {
+                            choices.push(
+                                new CharacterFeatAbilities(
+                                    {
+                                        name,
+                                    },
+                                    abilityImprovement.abilities,
+                                    abilityImprovement.points,
+                                ),
+                            );
+                        } else {
+                            fx.push({
+                                name: `${CharacterFeature.parseNameFromPageTitle(
+                                    name,
+                                )}: ${abilityImprovement.abilities[0]}`,
+                                type: GrantableEffectType.CHARACTERISTIC,
+                                subtype: GrantableEffectSubtype.ABILITY_FEAT,
+                                values: {
+                                    [abilityImprovement.abilities[0]]:
+                                        abilityImprovement.points,
+                                },
+                                // description: `Increases your ${abilityImprovement.abilities[0]} by ${abilityImprovement.points}.`,
+                                hidden: true,
+                            });
+                        }
                     }
 
                     return {
