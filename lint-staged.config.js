c406ab5b2c70a8d7b42c2510fb0d0125affec205
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-10-25 10:49:09 -0500

    Fix data dump linting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,8 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'eslint . --ext .js,.ts --cache',
-        (filenames) => `prettier --write ${filenames.join(' ')}`,
+        'npm run format',
         () => 'npm run type-check',
         'npm run test',
     ],
-    '*.{json}': (filenames) =>
-        filenames.length
-            ? `prettier --write ${filenames.join(' ')}`
-            : 'echo "No JSON files to format"',
 };
