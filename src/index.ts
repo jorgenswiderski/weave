7f3b59eca33ed2dc05e91e76c6c8b42f5e84b110
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-11-02 00:10:28 -0500

    Don't start express until the data is ready

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import { getCharacterClassData } from './models/character-class/character-class';
-import { log, warn } from './models/logger';
+import { debug, log, warn } from './models/logger';
 import { getCharacterRaceData } from './models/character-feature/features/character-race';
 import { getCharacterBackgroundData } from './models/character-feature/features/character-background';
 import { MwnProgressBar } from './api/mwn-progress-bar';
@@ -18,10 +18,8 @@ import { getEquipmentItemData } from './models/equipment/equipment';
 import { initActionsAndSpells } from './models/action/init';
 import { apiRouter } from './routes';
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-(async () => {
+async function main() {
+    new MwnProgressBar().render();
     await initActionsAndSpells();
 
     await Promise.all([
@@ -31,47 +29,48 @@ const PORT = process.env.PORT || 3001;
         getEquipmentItemData(),
     ]);
 
-    log('=============================================================');
-    warn('Weave is ready!');
-    log('=============================================================');
-})();
+    const app = express();
+    const PORT = process.env.PORT || 3001;
 
-new MwnProgressBar().render();
+    const allowedOrigins = [
+        'http://localhost:3000',
+        'https://tomekeeper.vercel.app',
+    ];
 
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://tomekeeper.vercel.app',
-];
+    app.use(
+        cors({
+            origin(origin, callback) {
+                // Allow requests with no origin (like mobile apps or curl requests)
+                if (!origin) return callback(null, true);
 
-app.use(
-    cors({
-        origin(origin, callback) {
-            // Allow requests with no origin (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    log(origin);
+                    const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
 
-            if (allowedOrigins.indexOf(origin) === -1) {
-                log(origin);
-                const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+                    return callback(new Error(msg), false);
+                }
 
-                return callback(new Error(msg), false);
-            }
+                return callback(null, true);
+            },
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
+            allowedHeaders: [
+                'Content-Type',
+                'Authorization',
+                'Baggage',
+                'Sentry-Trace',
+            ],
+        }),
+    );
 
-            return callback(null, true);
-        },
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
-        allowedHeaders: [
-            'Content-Type',
-            'Authorization',
-            'Baggage',
-            'Sentry-Trace',
-        ],
-    }),
-);
+    app.use(express.json());
+    app.use('/api', apiRouter);
 
-app.use(express.json());
+    app.listen(PORT, () => {
+        debug('=============================================================');
+        warn('Weave is ready!');
+        log(`Server is running on port ${PORT}`);
+        debug('=============================================================');
+    });
+}
 
-app.use('/api', apiRouter);
-
-app.listen(PORT, () => {
-    log(`Server is running on port ${PORT}`);
-});
+main();
