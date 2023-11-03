e398fc9ac09ca72a1edf8b973d9edc04386e8c95
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-11-02 22:16:14 -0500

    Add catch-all 500 error handler to all routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,19 @@
-import express, { Router } from 'express';
+import express, { ErrorRequestHandler, Router } from 'express';
 
 import { imageRouter } from './images';
 import { dataRouter } from './data';
 import { buildsRouter } from './builds';
+import { error } from '../models/logger';
 
 export const router: Router = express.Router();
+
+// Error handling middleware
+const errorHandler: ErrorRequestHandler = (err, req, res /* , next */) => {
+    error(err.stack);
+    res.status(500).send('Internal server error');
+};
+
+router.use(errorHandler);
 
 router.use('/data', dataRouter);
 router.use('/images', imageRouter);
