1af5f9a2a2922828fb15dd3a32e2a4c448298832
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-11-05 00:07:28 -0500

    Log uncaught exceptions and unhandled promise rejections to file.

--- src/models/logger.ts
+++ src/models/logger.ts
@@ -1,8 +1,11 @@
 import Bunyan from 'bunyan';
 import RotatingFileStream from 'bunyan-rotating-file-stream';
 import fs from 'fs';
+import path from 'path';
 
 fs.mkdirSync('logs', { recursive: true });
+
+const uncaughtExceptionLogPath = path.join('logs', 'weave-uncaught.log');
 
 const bunyan = Bunyan.createLogger({
     name: 'weave',
@@ -46,5 +49,39 @@ export class Logger {
     static error = bunyan.error.bind(bunyan);
     static warn = bunyan.warn.bind(bunyan);
 }
+
+process.on('uncaughtException', (err) => {
+    try {
+        // Bunyan doesn't like to finish flushing to disk before we kill the process, so let's take things into our own hands
+        const timeStamp = new Date().toISOString();
+
+        const errorLogMessage = `${timeStamp} - ${err.stack || err}\n`;
+
+        // Write the error synchronously to the 'weave-uncaught.log'
+        fs.writeFileSync(uncaughtExceptionLogPath, errorLogMessage, {
+            flag: 'a',
+        });
+    } catch (writeErr) {
+        // If there's an error writing to the log file, log it to the console
+        console.error(
+            'Error writing uncaught exception to log file:',
+            writeErr,
+        );
+    }
+
+    // Finally, exit the process
+    process.exit(1);
+});
+
+// Log uncaught promise rejections
+process.on('unhandledRejection', (reason) => {
+    if (reason instanceof Error) {
+        // If the reason is an error, Bunyan will serialize it properly.
+        Logger.error(reason, 'An uncaught promise rejection occurred');
+    } else {
+        // If it's not an error (like a string), you have to handle it manually.
+        Logger.error(reason, 'An uncaught promise rejection occurred');
+    }
+});
 
 export const { debug, log, error, warn } = Logger;
