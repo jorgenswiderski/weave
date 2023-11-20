a2d29bedbbc62288735c1d55401b47447a1d8efb
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-11-20 11:23:08 -0600

    Enable pre-commit type check again

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@ module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'eslint . --ext .js,.ts --cache',
         'npm run format',
-        // () => 'npm run type-check',
+        () => 'npm run type-check',
         'npm run test',
     ],
 };
